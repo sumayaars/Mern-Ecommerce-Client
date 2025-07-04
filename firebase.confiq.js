9316fbdd9d6f5a27d49a40fac463f8c6b57a456c
unknown <sumasumayaakter@gmail.com>
2025-07-04 14:09:16 +0600

    updated

--- firebase.confiq.js
+++ firebase.confiq.js
@@ -1,19 +1,15 @@
-// Import the functions you need from the SDKs you need
+// firebase.config.js
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCB7EUOrdDFDvZIWlvpxKRTIEvUu-RIQlA",
-  authDomain: "fullstack-ecommerce-70d30.firebaseapp.com",
-  projectId: "fullstack-ecommerce-70d30",
-  storageBucket: "fullstack-ecommerce-70d30.firebasestorage.app",
-  messagingSenderId: "353733402965",
-  appId: "1:353733402965:web:b8965137af2bbec1d50fdc"
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
